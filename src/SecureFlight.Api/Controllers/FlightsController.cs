d22b76a9514090258ec9edfe3f99879e6dfe9992
Lucas Custodio <lucas.custodio@thunders.com.br>
2022-03-21 09:27:51 -0300

    Add project files.

---
+++ src/SecureFlight.Api/Controllers/FlightsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecureFlight.Api.Models;
+using SecureFlight.Api.Utils;
+using SecureFlight.Core.Entities;
+using SecureFlight.Core.Interfaces;
+
+namespace SecureFlight.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightsController : ControllerBase
+    {
+        private readonly IService<Flight> _flightService;
+
+        public FlightsController(IService<Flight> flightService)
+        {
+            _flightService = flightService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<FlightDataTransferObject>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponseActionResult))]
+        public async Task<IActionResult> Get()
+        {
+            var flights = (await _flightService.GetAllAsync()).Result
+                .Select(x => new FlightDataTransferObject
+                {
+                    Id = x.Id,
+                    ArrivalDateTime = x.ArrivalDateTime,
+                    Code = x.Code,
+                    FlightStatusId = (int) x.FlightStatusId,
+                    DepartureDateTime = x.DepartureDateTime,
+                    DestinationAirport = x.DestinationAirport,
+                    OriginAirport = x.OriginAirport
+                });
+
+            return Ok(flights);
+        }
+    }
+}
---
+++ src/SecureFlight.Api/Controllers/PassengersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecureFlight.Api.Models;
+using SecureFlight.Api.Utils;
+using SecureFlight.Core.Entities;
+using SecureFlight.Core.Interfaces;
+
+namespace SecureFlight.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PassengersController : ControllerBase
+    {
+        private readonly IService<Passenger> _personService;
+
+        public PassengersController(IService<Passenger> personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<PassengerDataTransferObject>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponseActionResult))]
+        public async Task<IActionResult> Get()
+        {
+            var passengers = (await _personService.GetAllAsync()).Result
+                .Select(x => new PassengerDataTransferObject
+                {
+                    Email = x.Email,
+                    FirstName = x.FirstName,
+                    Id = x.Id,
+                    LastName = x.LastName
+                });
+
+            return Ok(passengers);
+        }
+    }
+}
